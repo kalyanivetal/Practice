1d6ee52a3610abf8892e817d77944dd0d0712c2e
kalyanivetal <Anand-Chavan>
2019-08-06 12:53:42 +0530

    updated All

--- Practice_PP/try1.c
+++ Practice_PP/try1.c
@@ -4,23 +4,18 @@
  void  arr_bday(int [],int);
  void arr_bday(int q[],int q_count)
 {
-int i=0,j=0,temp=0;
-int cnt=0;
+	int i=0,j=0,temp=0;
+	int cnt=0,key=0;
 
-for(i=0;i<q_count;i++)
-    {
-        if((q[i]-(i+1))>=3)
-        {
-            printf("Too chaotic\n");
-		return;	
-        }
-    /*    else if((q[i]-(i))>3)
-        {
-         printf("Too chaotic");
-            break;   
-        } */
-    }
-    for(i=0;i<q_count;i++)
+	for(i=0;i<q_count;i++)
+    	{
+        	if((q[i]-(i+1))>=3)
+	        {	
+    	        printf("Too chaotic\n");
+				return;	
+	        }
+    	}
+   /* for(i=0;i<q_count;i++)
     {
         for(j=i+1;j<q_count;j++)
         {
@@ -33,6 +28,20 @@ for(i=0;i<q_count;i++)
             }
         }
     }
+	*/
+    for(i=1;i<q_count; i++) 
+	{
+      	  key=q[i];
+          j=i-1;
+
+      	while(j>=0&&q[j]>key)
+	  	{
+        	    q[j+1]=q[j];
+            	j=j-1;
+				cnt++;
+    	}
+      	  q[j+1]=key;
+    }
 printf("%d\n",cnt);
 }
 int main()
@@ -41,24 +50,24 @@ int main()
  	int *p1=NULL;
 	printf("Enter N:");
 	scanf("%d",&n);
-	for(j=0;j<n;j++)
-	{
-	printf("Enter No. of Elements in ARRAY: \n");
-	scanf("%d",&iLength1);
+//	for(j=0;j<n;j++)
+//	{
+		printf("Enter No. of Elements in ARRAY: \n");
+		scanf("%d",&iLength1);
 
-	p1=(int *)malloc(iLength1*sizeof(int));
-	if(p1==NULL)
-	{
-    		return -1;
-	}
+		p1=(int *)malloc(iLength1*sizeof(int));
+		if(p1==NULL)
+		{
+    			return -1;
+		}
 
-	printf("Enter Elements of ARRAY: \n");
-	for(i=0;i<iLength1;i++)
-	{
-    		scanf("%d",&p1[i]);
-	}
-	arr_bday(p1,iLength1);
-	}
+		printf("Enter Elements of ARRAY: \n");
+		for(i=0;i<iLength1;i++)
+		{
+    			scanf("%d",&p1[i]);
+		}
+		
+		arr_bday(p1,iLength1);
+//	}
 	return 0;
-
 }
